f2b8115adfead0dd8f881d6375f1b73e00e12d6a
Leonardo Simonetti <leosimonetti@hotmail.com>
2020-07-05 11:21:31 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroPF/Classes/Classe_PessoaFisica.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroPF.Classes
+{
+    class Classe_PessoaFisica
+    {
+        private int id;
+        private String nome;
+        private DateTime dataNasc;
+        private String rg;
+        private String telefone;
+        private String endereco;
+
+        public int ID { get { return id; } set { id = value; } }
+        public String Nome { get { return nome; } set { nome = value; } }
+        public DateTime DataNasc { get { return dataNasc; } set { dataNasc = value; } }
+        public String RG { get { return rg; } set { rg = value; } }
+        public String Telefone { get { return telefone; } set { telefone = value; } }
+        public String Endereco { get { return endereco; } set { endereco = value; } }
+
+        public Classe_PessoaFisica()
+        {
+            id = 0;
+            nome = "";
+            dataNasc = DateTime.Today;
+            rg = "";
+            telefone = "";
+            endereco = "";
+        }
+
+    }
+}
---
+++ CadastroPF/Form3.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CadastroPF.Classes;
+using Microsoft.VisualBasic;
+
+namespace CadastroPF
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Classes.Classe_PessoaFisica pessoaFisica = new Classes.Classe_PessoaFisica();
+
+            pessoaFisica.ID = Int32.Parse(textBox1.Text);
+            pessoaFisica.Nome = textBox2.Text;
+
+            if (IsDate(textBox3.Text))
+            {
+                pessoaFisica.DataNasc = DateTime.Parse(textBox3.Text);
+            } else
+            {
+                MessageBox.Show("Erro. Digite uma data correta no formato dd/mm/aaaa.");
+                return;
+            }
+
+            pessoaFisica.RG = textBox4.Text;
+            pessoaFisica.Telefone = textBox5.Text;
+            pessoaFisica.Endereco = textBox6.Text;
+
+            // salvar no banco de dados
+
+
+        }
+
+        public static bool IsDate(Object obj)
+        {
+            string strDate = obj.ToString();
+            try
+            {
+                DateTime dt = DateTime.Parse(strDate);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
